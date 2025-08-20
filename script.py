99614648a92d23d184f0ffa5378d2e4b84c61d6d
Gajender <Iamsanju0707@gmail.com>
2025-08-20 10:33:27 +0530

    adding the files

--- script.py
+++ script.py
@@ -1,1 +1,12 @@
 print("this is the demo file")
+#6. Find all prime numbers up to N
+n=20
+for i in range(1,n+1):
+    k=0
+    for j in range(2,i):
+        if i%j==0:
+            k=k+1
+    if k==0:
+        print(f" {i} is Prime")
+    else:
+        print(f"{i} is  not Prime")
